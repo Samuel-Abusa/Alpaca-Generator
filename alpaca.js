f8eddf430f8d21f5c5fd9e5d6fc72793101170f4
Samuel-Abusa <samuelabusakennedy@gmail.com>
2021-10-18 13:55:32 +0300

    added the needed accessories and aligned them accordinly

---
+++ alpaca.js
@@ -0,0 +1,17 @@
+"use strict";
+// Alpaca
+const neck = document.querySelector(".neck");
+const mouth = document.querySelector(".mouth");
+const hair = document.querySelector(".hair");
+const leg = document.querySelector(".leg");
+const ears = document.querySelector(".ears");
+const eyes = document.querySelector(".eyes");
+// Buttons
+const hairButton = document.querySelector(".hairB");
+const earsB = document.querySelector(".earsB");
+const eyesB = document.querySelector(".eyesB");
+const mouthB = document.querySelector(".mouthB");
+const neckB = document.querySelector(".neckB");
+const legB = document.querySelector(".legB");
+const accessoriesB = document.querySelector(".accessoriesB");
+const backgroundB = document.querySelector(".backgroundB");
